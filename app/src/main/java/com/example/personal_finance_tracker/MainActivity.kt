80f1a89d2db495ffa2380a31f864bc4f613900d1
samaahmed19 <sama23012612@gmail.com>
2025-08-02 02:14:37 +0300

    update main activity and xml+colors/dimens

--- app/src/main/java/com/example/personal_finance_tracker/MainActivity.kt
+++ app/src/main/java/com/example/personal_finance_tracker/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.personal_finance_tracker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvWelcome: TextView
+    private lateinit var btnTrack: Button
+    private lateinit var btnShare: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        tvWelcome = findViewById(R.id.welcomTv)
+        btnTrack = findViewById(R.id.btnTrack)
+        btnShare = findViewById(R.id.btnShare)
+
     }
 }
